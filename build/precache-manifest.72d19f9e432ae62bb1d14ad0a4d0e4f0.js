196b926867c5539bdbe66a1ea3657688e0c13643
herivillaz <herivillaz@gmail.com>
2020-09-18 11:49:41 -0400

    api

---
+++ build/precache-manifest.72d19f9e432ae62bb1d14ad0a4d0e4f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec16dead50daf0affebb541f7743bf5",
+    "url": "/Employee-Directory-React/index.html"
+  },
+  {
+    "revision": "36ebb92f7c0bc0214845",
+    "url": "/Employee-Directory-React/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "36ebb92f7c0bc0214845",
+    "url": "/Employee-Directory-React/static/js/2.60a242b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory-React/static/js/2.60a242b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b3559da7e96ef24b181",
+    "url": "/Employee-Directory-React/static/js/main.c116508d.chunk.js"
+  },
+  {
+    "revision": "5ba0ae06a6a341e9abc1",
+    "url": "/Employee-Directory-React/static/js/runtime-main.637e7d54.js"
+  }
+]);
