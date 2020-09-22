3e7624b850b79682cc14664b70f10d61ae4f1f0f
herivillaz <herivillaz@gmail.com>
2020-09-22 18:53:36 -0400

    button

--- src/components/OmdbContainer.js
+++ src/components/OmdbContainer.js
@@ -32,47 +32,55 @@ class OmdbContainer extends Component {
     }, () => console.log(this.state));
   };
 
-  // When the form is submitted, search the OMDB API for the value of `this.state.search`
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  //   this.searchMovies(this.state.search);
-  // };
+  // const directory = state.result;
+  // directory.click(() => {
+  //   directory.sort();
+  //   console.log(directory);
+  // });
+    
+  
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col size="md-8">
-            <Card
-              heading={this.state.result.Title || "Employee Directory"}
-            >
-              {this.state.result.length ? 
-                this.state.result
-                  .filter(item => 
-                    `${item.name.first.toLowerCase()} ${item.name.last.toLowerCase() }`.includes(this.state.search.toLowerCase())
-                  )
-                  .map(item => (
-                    <div className="list-group-item justify-content-between" key={item.id.value}>
-                     {item.name.first}  {item.name.last}        {item.phone}     {item.email}
-                    </div>
-                  )
-                  ) : (
-                    <h3>No Results to Display</h3>
-                  )}
-            </Card>
-          </Col>
-          <Col size="md-4">
-            <Card heading="Search">
-              <SearchForm
-                value={this.state.search}
-                handleInputChange={this.handleInputChange}
-              />
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+// When the form is submitted, search the OMDB API for the value of `this.state.search`
+// handleFormSubmit = event => {
+//   event.preventDefault();
+//   this.searchMovies(this.state.search);
+// };
+
+render() {
+  return (
+    <Container>
+      <Row>
+        <Col size="md-8">
+          <Card
+            heading={this.state.result.Title || "Employee Directory"}
+          >
+            {this.state.result.length ?
+              this.state.result
+                .filter(item =>
+                  `${item.name.first.toLowerCase()} ${item.name.last.toLowerCase()}`.includes(this.state.search.toLowerCase())
+                )
+                .map(item => (
+                  <div className="list-group-item justify-content-between" key={item.id.value}>
+                    {item.name.first}  {item.name.last}        {item.phone}     {item.email}
+                  </div>
+                )
+                ) : (
+                <h3>No Results to Display</h3>
+              )}
+          </Card>
+        </Col>
+        <Col size="md-4">
+          <Card heading="Search">
+            <SearchForm
+              value={this.state.search}
+              handleInputChange={this.handleInputChange}
+            />
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 }
 
 export default OmdbContainer;
